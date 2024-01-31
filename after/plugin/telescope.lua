1186e1f1700280cab0e0036404293b81e1552b04
Lumikrynd <WhatIsEmail?>
2024-01-31 23:03:17 +0100

    fun with telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,51 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+
+local fileSettings = {
+  --hidden = true,
+}
+
+local function Unhide(func)
+  local function inner()
+    func(fileSettings)
+  end
+
+  return inner
+end
+
+local function better_live_grep()
+  builtin.live_grep {
+    glob_pattern = {
+      --"!**/.git/*",
+      "!**/.git/logs/**",
+      "!**/.git/objects/**",
+      "!**/.git/refs/**",
+      --"!**.lua"
+      --see https://github.com/BurntSushi/ripgrep/blob/master/GUIDE.md#configuration-file
+    },
+    additional_args = {
+      "--hidden"
+    },
+  }
+end
+
+vim.keymap.set('n', '<leader>ff', Unhide(builtin.find_files), {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fr', better_live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ft', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fr', builtin.live_grep, {})
+
+require('telescope').setup{
+  defaults = {
+    file_ignore_patterns = {
+      --"%.git",
+      --"%.git\\objects\\",
+      --"%.git\\refs\\",
+      --"%.git\\logs\\",
+      --"%.git\\[%w_]*",
+    }
+  },
+}
+
+
+--{hidden=true, layout_config={prompt_position="top"}}
+--builtin.man_pages
