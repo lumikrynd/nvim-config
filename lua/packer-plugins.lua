f775ca76c31ae5cf9930e143bf20e1ac122868e5
Lumikrynd <WhatIsEmail?>
2024-01-31 19:07:31 +0100

    remove broken and unused packages

--- lua/packer-plugins.lua
+++ lua/packer-plugins.lua
@@ -31,12 +31,13 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1', -- or , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5', -- or , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-    use('nvim-treesitter/playground')
-
-    use('mbbill/undotree')
+    use {
+      'nvim-treesitter/nvim-treesitter',
+      {run = ':TSUpdate'}
+    }
 end)
