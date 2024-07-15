be8cb9dec7563d65101e4a1bce4cb437aa5f168b
Lumikrynd <WhatIsEmail?>
2024-07-15 21:42:24 +0200

    Moved package definitions to own file

--- lua/package-manager/init.lua
+++ lua/package-manager/init.lua
@@ -3,19 +3,7 @@ require(... .. ".bootstrap")
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- add your plugins here
-    {
-      "nvim-telescope/telescope.nvim",
-      tag = '0.1.8',
-      dependencies = {
-        "nvim-lua/plenary.nvim"
-      }
-    },
-    {
-      "nvim-treesitter/nvim-treesitter",
-      tag = 'v0.9.2'--,
-      --{build = ':TSUpdate'}
-    }
+    { import = ... .. ".plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
---
+++ lua/package-manager/plugins.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = '0.1.8',
+    dependencies = {
+      "nvim-lua/plenary.nvim"
+    }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    tag = 'v0.9.2'--,
+    --{build = ':TSUpdate'}
+  }
+}
