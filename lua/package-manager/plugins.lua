afc10c85de55564038b16af5f9ccaf2796da91fc
Lumikrynd <WhatIsEmail?>
2024-10-19 12:20:34 +0200

    Remove versions from packages
    Fixes treesitter problems...

--- lua/package-manager/plugins.lua
+++ lua/package-manager/plugins.lua
@@ -1,14 +1,14 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    tag = '0.1.8',
+    --tag = '0.1.8',
     dependencies = {
       "nvim-lua/plenary.nvim"
     }
   },
   {
     "nvim-treesitter/nvim-treesitter",
-    tag = 'v0.9.2'--,
+    --tag = 'v0.9.2'--,
     --{build = ':TSUpdate'}
   }
 }
