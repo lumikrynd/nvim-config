cb64de6a8a542683a257b1d76b0e44a1851b84ac
Lumikrynd <WhatIsEmail?>
2025-03-11 23:19:13 +0100

    oil.nvim file explorer

--- lua/package-manager/plugins.lua
+++ lua/package-manager/plugins.lua
@@ -11,6 +11,14 @@ return {
     --{build = ':TSUpdate'}
   },
   {
+    'stevearc/oil.nvim',
+    opts = {
+      default_file_explorer = true, --it's here if I need to turn it off
+    },
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    lazy = false,
+  },
+  {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
