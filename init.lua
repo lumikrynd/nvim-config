f9e4aa84d468220a7951a6a9a8260ecb92299c0e
Lumikrynd <WhatIsEmail?>
2025-03-11 19:49:05 +0100

    Modify color scheme

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
 --print("Hello world")
 require("settings")
 require("package-manager")
-
-vim.cmd("colorscheme tokyonight-night")
--- lua/package-manager/plugins.lua
+++ lua/package-manager/plugins.lua
@@ -11,7 +11,25 @@ return {
     --tag = 'v0.9.2'--,
     --{build = ':TSUpdate'}
   },
-  "folke/tokyonight.nvim",
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+      styles = {
+        functions = {}
+      },
+      on_colors = function(colors)
+        -- See https://github.com/folke/tokyonight.nvim/blob/main/extras/lua/tokyonight_night.lua for original colors
+        colors.border = "#333333"
+        colors.bg_statusline = "#333333"
+      end
+    },
+    init = function()
+      vim.cmd([[colorscheme tokyonight-night]])
+    end,
+  },
   {
     "christoomey/vim-tmux-navigator",
     cmd = {
