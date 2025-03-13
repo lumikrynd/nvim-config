5578e209b1d74b38acbb9d061bbf479a4a6a1a69
Lumikrynd <WhatIsEmail?>
2025-03-13 19:42:39 +0100

    Warning message stuff

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
---print("Hello world")
+--Theme seems to hide if only a single print is used)
+--Fix by always printing this ¯\_(ツ)_/¯
+print("Any Warnings?")
+
 require("settings")
 require("package-manager")
