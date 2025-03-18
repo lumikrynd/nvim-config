0208381d3b4c76b60d76d0d70f8d690cfaa1c3fc
Lumikrynd <WhatIsEmail?>
2025-03-18 19:09:28 +0100

    Oil - git mv integration

--- lua/package-manager/plugins/oil.lua
+++ lua/package-manager/plugins/oil.lua
@@ -2,6 +2,10 @@ return {
   'stevearc/oil.nvim',
   opts = {
     default_file_explorer = true, --it's here if I need to turn it off
+    git = {
+      -- Return true to automatically git add/mv/rm files
+      mv = function(src_path, dest_path) return true end,
+    },
   },
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   lazy = false,
