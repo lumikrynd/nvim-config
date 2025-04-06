2542daba126e3c81fa75e1c7dc7374aff85fe839
Lumikrynd <WhatIsEmail?>
2025-04-06 22:13:07 +0200

    playing with lsp settings
    - autocomplete
    - diagnostics

--- lua/lsp-setup/init.lua
+++ lua/lsp-setup/init.lua
@@ -1,1 +1,18 @@
 require(... .. '.luals')
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
+    end
+  end,
+})
+
+vim.cmd[[set completeopt+=menuone,noselect]]
+
+vim.diagnostic.config({
+  virtual_lines = true
+})
