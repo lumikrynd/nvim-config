0197b229dd7a9b926f8dae03c33a9757f616b7c4
Lumikrynd <WhatIsEmail?>
2025-04-14 14:41:58 +0200

    Changes to csharp lsp
    - Changed how the root was found by using functions nvim-lspconfig
    - Now support both omnisharp and omnisharp-mono (one being the one
      normally running on windows and the other being the one normally
      running on Linux... I think)

--- lua/lsp-setup/csharp-ls.lua
+++ lua/lsp-setup/csharp-ls.lua
@@ -1,9 +1,21 @@
-local pid = vim.fn.getpid();
-vim.lsp.config['csharp'] = {
-  cmd = { 'omnisharp-mono', '--languageserver', '--hostPID', tostring(pid) },
-  filetypes = { 'cs' },
-  root_markers = { '.git' }, --haven't found a wildcard option
-  settings = { },
-}
+local util = require 'lspconfig.util'
 
+local pid = vim.fn.getpid();
+
+local function CreateConfig (name)
+  return {
+    cmd = {name, '--languageserver', '--hostPID', tostring(pid) },
+    filetypes = { 'cs' },
+    root_dir = function(bufnr, on_dir)
+      local fname = vim.api.nvim_buf_get_name(bufnr)
+      on_dir(util.root_pattern '*.sln'(fname) or util.root_pattern '*.csproj'(fname))
+    end,
+    settings = { },
+  }
+end
+
+vim.lsp.config['csharp'] = CreateConfig('omnisharp-mono')
 vim.lsp.enable('csharp')
+
+vim.lsp.config['csharp-windows'] = CreateConfig('omnisharp')
+vim.lsp.enable('csharp-windows')
