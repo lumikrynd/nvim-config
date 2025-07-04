7fbfef4c6177e80d088c7ea66f18a060805a5b25
Lumikrynd <WhatIsEmail?>
2025-07-04 07:32:47 +0200

    Telescope: cleaning up some of the old config

--- lua/package-manager/plugins/telescope.lua
+++ lua/package-manager/plugins/telescope.lua
@@ -1,9 +1,8 @@
-local telescope, builtin
-
-local function better_live_grep() end
-
-local function set_better_live_grep()
-  function better_live_grep()
+local function get_better_live_grep()
+  -- Can't for the love of god figure out how to pass in builtin's type if given as an argument
+  -- Just fetch it here instead for better autocomplete
+  local builtin = require("telescope.builtin")
+  return function()
     builtin.live_grep {
       glob_pattern = {
         "!**/.git/COMMIT_EDITMSG",
@@ -56,22 +55,16 @@ return {
     "nvim-lua/plenary.nvim"
   },
   config = function()
-    local tstatus,bstatus
-    tstatus,telescope = pcall(require, 'telescope')
-    bstatus,builtin = pcall(require, 'telescope.builtin')
-
-    if not (tstatus and bstatus) then
-      print("telescope configuration skipped")
-      return
-    end
+    local telescope = require("telescope")
+    local builtin = require("telescope.builtin")
 
     telescope.setup(opts)
 
-    set_better_live_grep()
+    local livegrep = get_better_live_grep()
 
     vim.keymap.set("n", "<Plug>telescope-find_files", Unhide(builtin.find_files), {})
     vim.keymap.set("n", "<Plug>telescope-git_files", builtin.git_files, {})
-    vim.keymap.set("n", "<Plug>telescope-live_grep", better_live_grep, {})
+    vim.keymap.set("n", "<Plug>telescope-live_grep", livegrep, {})
     vim.keymap.set("n", "<Plug>telescope-buffers", builtin.buffers, {})
     vim.keymap.set("n", "<Plug>telescope-help_tags", builtin.help_tags, {})
 
